f76771f28cf69a8f06ad9ed9fe28db3a173901fa
Maymisson <maymisson@gmail.com>
2020-01-22 23:52:23 -0300

    Manipulando style com JS (var.style)

--- JAVASCRIPT/introducao-javascript/js/app.js
+++ JAVASCRIPT/introducao-javascript/js/app.js
@@ -6,8 +6,8 @@ var titulo = document.querySelector("h1");
 //-- criando variavel para manipular todos os elementos que pussui a classe CSS (.paciente) retornando array
 var pacientes = document.querySelectorAll(".paciente");
 
-// -- loop for percorrendo o tamanho de uma array (paciente.length)
-for (var i = 0; i < paciente.length; i++) {
+// -- loop for percorrendo o tamanho de uma array (pacientes.length)
+for (var i = 0; i < pacientes.length; i++) {
 
     // -- Fazendo com que a variavel percorra um item por vez da array (var = var[i])  
     var paciente = pacientes[i];
@@ -22,21 +22,24 @@ for (var i = 0; i < paciente.length; i++) {
 
     var tdImc = paciente.querySelector(".info-imc");
 
+    // - criando variaveis booleanas para a condição
     var pesoValidado = true;
     var alturaValidado = true;
 
     // -- Estrutura de condição e operadores logicos
 
     if (peso <= 0 || peso > 300) {
-        //console.log("Peso inválido"); 
+        console.log("Peso inválido"); 
         pesoValidado = false;
-        //tdImc.textContent = "Peso inválido!"; -- mensagem individual para peso inválido 
+        tdImc.textContent = "Peso inválido!"; //-- mensagem individual para peso inválido
+        paciente.style.backgroundColor = "lightcoral";
     }
 
     if (altura <= 0 || altura > 3) {
-        //console.log("Altura inválida");
+        console.log("Altura inválida");
         alturaValidado = false;
-        //tdImc.textContent = "Altura inválida!"; -- mensagem individual para altura inválido
+        tdImc.textContent = "Altura inválida!"; //-- mensagem individual para altura inválido
+        paciente.style.backgroundColor = "lightcoral";
     }
 
     if (pesoValidado && alturaValidado) {
@@ -44,9 +47,10 @@ for (var i = 0; i < paciente.length; i++) {
         var imc = peso / (altura * altura);
         //usando limitado (toFixed) decimal para float
         tdImc.textContent = imc.toFixed(2);
-    } else {
-        tdImc.textContent = "Peso e/ou altura inválidos!";
-    }
+    } //else {
+        //tdImc.textContent = "Peso e/ou altura inválidos!"; //-- mensagem para generalizar quando uma das condições forem invalidas
+        //paciente.style.backgroundColor = "lightcoral";
+    //}
 }
 
 
