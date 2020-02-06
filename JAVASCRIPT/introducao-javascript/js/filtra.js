6fc99c7d6da70d5411400d3380ca8656083e24bf
Maymisson <maymisson@gmail.com>
2020-02-06 17:00:35 -0300

    apagando class CSS caso apague o text de busca

--- JAVASCRIPT/introducao-javascript/js/filtra.js
+++ JAVASCRIPT/introducao-javascript/js/filtra.js
@@ -5,16 +5,26 @@ campoFiltro.addEventListener("input", function(){
     //-- comparando os nomes da tabela
     let pacientes = document.querySelectorAll(".paciente");
     //-- Condição de busca
-    for (let i = 0; i < pacientes.length; i++) {
-        let paciente = pacientes[i];
-        let tdNome = paciente.querySelector(".info-nome");
-        let nome = tdNome.textContent;
-
-        //-- Implementando lógica de filtragem
-        if(nome != this.value){
-            paciente.classList.add("invisivel")
-        }else {
+    if (this.value.length > 0){
+        //-- Acessando nome dos pacientes e também ao conteúdo de texto do filtro
+        for (let i = 0; i < pacientes.length; i++) {
+            let paciente = pacientes[i];
+            let tdNome = paciente.querySelector(".info-nome");
+            let nome = tdNome.textContent;
+    
+            //-- Implementando lógica de filtragem
+            if(nome != this.value){
+                paciente.classList.add("invisivel")
+            }else {
+                paciente.classList.remove("invisivel");
+            }
+        }
+        //-- apagando classe invisivel caso apague o texto do campo de busca
+    } else {
+        for (let i = 0; i < pacientes.length; i++){
+            let paciente = pacientes[i];
             paciente.classList.remove("invisivel");
         }
     }
+    
 });
